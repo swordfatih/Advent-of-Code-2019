e055be9eccd20ec55c8cdfd01d161c4ab6af2ce7
swordfatih <35528737+swordfatih@users.noreply.github.com>
2019-12-04 20:41:20 +0100

    Day 1 part 2

--- main.cpp
+++ main.cpp
@@ -40,21 +40,55 @@ std::vector<Type> get_input_list(std::filesystem::path path)
 /////////////////////////////////////////////////
 // Challenges
 /////////////////////////////////////////////////
-uint32_t mass_calculator(std::vector<uint32_t> inputs)
+int64_t fuel_calculator(int64_t mass)
+{
+    return std::floor(mass / 3) - 2;
+}
+
+/////////////////////////////////////////////////
+int64_t fuel_requirement(std::vector<int64_t> inputs)
 {
     if(inputs.empty())
     {
         return 0;
     }
 
-    uint32_t result = 0;
+    int64_t fuel = 0;
 
     for(const auto& mass: inputs)
     {
-        result += std::floor(mass / 3) - 2;
+        fuel += fuel_calculator(mass);
     }
 
-    return result;
+    return fuel;
+}
+
+/////////////////////////////////////////////////
+int64_t total_fuel_requirement(std::vector<int64_t> inputs)
+{
+    if(inputs.empty())
+    {
+        return 0;
+    }
+
+    int64_t fuel = 0;
+
+    for(const auto& mass: inputs)
+    {
+        auto remaining_mass = mass;
+
+        while(remaining_mass > 0)
+        {
+            remaining_mass = fuel_calculator(remaining_mass);
+
+            if(remaining_mass > 0)
+            {
+                fuel += remaining_mass;
+            }
+        }
+    }
+
+    return fuel;
 }
 
 /////////////////////////////////////////////////
@@ -64,7 +98,8 @@ int main()
 {
     std::vector<std::variant<std::string, int64_t>> answers;
 
-    answers.push_back(mass_calculator(get_input_list<uint32_t>("inputs/01-mass_input.txt")));
+    answers.push_back(fuel_requirement(get_input_list<int64_t>("inputs/01-mass_input.txt")));
+    answers.push_back(total_fuel_requirement(get_input_list<int64_t>("inputs/01-mass_input.txt")));
 
     std::ofstream writer("output.txt");
     for(uint16_t day = 0; day < answers.size(); ++day)
@@ -78,7 +113,7 @@ int main()
 
             if(writer)
             {
-                writer << output.str();
+                writer << output.str() << '\n';
             }
         }, answers[day]);
     }
